1d08530cdf30d8b480d662279e36594540560885
Muhammad Kashif <mkashif.aw@gmail.com>
2023-04-15 17:10:53 +0500

    Initial commit

---
+++ app/about/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './style.module.css';
+export default function AboutLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <nav>About Navbar</nav>
+      <main className={styles.main}>{children}</main>
+    </>
+  );
+}
